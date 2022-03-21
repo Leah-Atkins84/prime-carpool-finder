501bb807c1ba93f3f0519c6f1fb2b252ef985cc3
Leah Atkins <leah.atkins84@gmail.com>
2022-03-21 14:16:54 -0500

    Added code comments to delete account page

--- src/components/DeleteAccountButton/DeleteAccountButton.jsx
+++ src/components/DeleteAccountButton/DeleteAccountButton.jsx
@@ -1,4 +1,4 @@
-import React,  {useState} from 'react';
+import React, {useState} from 'react';
 import {useDispatch, useSelector} from 'react-redux';
 import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
@@ -10,72 +10,73 @@ import DialogContent from '@mui/material/DialogContent';
 import DialogContentText from '@mui/material/DialogContentText';
 import DialogTitle from '@mui/material/DialogTitle';
 
-// import AccessAlarmIcon from '@mui/icons-material/AccessAlarm'
-
 const theme = createTheme({
-  palette: {
-      primary: {
-          main: '#26a69a'
-      },
-      secondary: {
-          main: '#1CCFCF'
-      }
-  }
+    palette: {
+        primary: {
+            main: '#26a69a'
+        },
+        secondary: {
+            main: '#1CCFCF'
+        }
+    }
 });
 
 
 function DeleteAccountButton() {
-  const user = useSelector((store) => store.user); // use data from redux
-  const dispatch = useDispatch();
+    const user = useSelector((store) => store.user); // use data from redux
+    const dispatch = useDispatch();
 
-  const [open, setOpen] = useState(false);
+    const [open, setOpen] = useState(false);
 
-  const handleClickOpen = () => {
-    setOpen(true);
-  };
+    const handleClickOpen = () => {
+        setOpen(true); /* Opens dialog alert box */
+    };
 
-  const handleClose = () => {
-    setOpen(false);
-  };
+    const handleClose = () => {
+        setOpen(false); /* Closes dialog box */
+    };
 
-  const deleteUser = () => { 
-    return dispatch({
-      type: 'DELETE_ACCOUNT', 
-      payload: {
-        id: user.id
-      }
-    })
-  }
+    const deleteUser = () => {
+        return dispatch({
+            type: 'DELETE_ACCOUNT', /* Dispatches to delete saga when agree is clicked on dialog box */
+            payload: {
+                id: user.id
+            }
+        })
+    }
     return (
-      <ThemeProvider theme={theme}>
-      <Stack spacing={2} direction="row">
-        <Button size="small" variant="contained" color="secondary" 
-                onClick={handleClickOpen}>
-                DELETE ACCOUNT
-                <DeleteIcon />
-        </Button>
-        <Dialog
-          open={open}
-          onClose={handleClose}
-          aria-labelledby="alert-dialog-title"
-          aria-describedby="alert-dialog-description">
-            <DialogTitle id="alert-dialog-title">
-              {"Are you sure you want to delete your account?"}  
-            </DialogTitle>
-            <DialogContent>
-              <DialogContentText id="alert-dialog-description">
-                Deleting your account means that you will no longer be able to see the carpool list and no one else will be able to view your ride status.
-              </DialogContentText>
-            </DialogContent>
-            <DialogActions>
-              <Button onClick={handleClose}>Disagree</Button>
-              <Button onClick={deleteUser} autoFocus>
-                Agree
-              </Button>
-            </DialogActions> 
-        </Dialog>
-      </Stack>
-      </ThemeProvider>
+        <ThemeProvider theme={theme}>
+            <Stack spacing={2}
+                direction="row">
+                <Button size="small" variant="contained" color="secondary"
+                    onClick={handleClickOpen}>
+                    {/* Opens dialog box when button clicked */}
+                    DELETE ACCOUNT
+                    <DeleteIcon/>
+                </Button>
+                <Dialog open={open}
+                    onClose={handleClose}
+                    aria-labelledby="alert-dialog-title"
+                    aria-describedby="alert-dialog-description">
+                    <DialogTitle id="alert-dialog-title">
+                        {"Are you sure you want to delete your account?"} </DialogTitle>
+                    <DialogContent>
+                        <DialogContentText id="alert-dialog-description">
+                            Deleting your account means that you will no longer be able to see the carpool list and no one else will be able to view your ride status.
+                        </DialogContentText>
+                    </DialogContent>
+                    <DialogActions>
+                        <Button onClick={handleClose}>Disagree</Button>
+                        {/* Closes dialog box */}
+                        <Button onClick={deleteUser}
+                            autoFocus>
+                            {/* Deletes user account when clicked */}
+                            Agree
+                        </Button>
+                    </DialogActions>
+                </Dialog>
+            </Stack>
+        </ThemeProvider>
     )
 }
 

a063302a2636dfc6919e723c0eabfe01440f2140
Leah Atkins <leah.atkins84@gmail.com>
2022-03-21 16:45:54 -0500

    Deleted 2 component files and routes/links

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -27,10 +27,6 @@ function Nav() {
             <Link className="navLink" to="/user">
               Home
             </Link>
-
-            <Link className="navLink" to="/info">
-              Info Page
-            </Link>
             <Link className="navLink" to="/carpool">
              Carpool List
             </Link>
